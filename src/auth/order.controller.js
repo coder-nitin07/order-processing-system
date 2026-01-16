8c76aa9dfecb4d2e497a1019a8128e5a5e229f49
coder-nitin07 <nitinthakur87504@gmail.com>
2026-01-16 14:41:42 +0530

    crete order apis

--- src/auth/order.controller.js
+++ src/auth/order.controller.js
@@ -2,6 +2,7 @@ import pool from '../database/postgres.js';
 import ORDER_STATUS from "../constants/orderStatus.js";
 import { createOrderSchema } from "../validators/order.validator.js";
 
+// create order
 export const createOrder = async (req, res, next)=>{
     const client = await pool.connect();
 
@@ -40,5 +41,66 @@ export const createOrder = async (req, res, next)=>{
         next(err);
     } finally{
         client.release();
+    }
+};
+
+// get orders
+export const getOrders = async (req, res, next)=>{
+    try {
+        const { userId, role } = req.user;
+        
+        let query;
+        let values;
+
+        if(role == 'ADMIN'){
+            query = "SELECT * FROM orders ORDER BY created_at DESC";
+            values = [];
+        } else{
+            query = `
+                SELECT * FROM orders
+                WHERE user_id = $1
+                ORDER BY created_at DESC
+            `;
+
+            values = [ userId ];
+        }
+
+        const result = await pool.query(query, values);
+        
+        res.status(200).json({ order: result.rows });
+    } catch (err) {
+        next(err);
+    }
+};
+
+// get orders By Id
+export const getOrderById = async (req, res, next)=>{
+    try {
+        const { userId, role } = req.user;
+        const orderId = req.params.id;
+
+        let query;
+        let values;
+        
+        if(role === 'ADMIN'){
+            query = "SELECT * FROM orders WHERE id = $1";
+            values = [ orderId ];
+        } else{
+            query = `
+                SELECT * FROM orders
+                WHERE id = $1 AND user_id = $2
+            `;
+            values = [orderId, userId];
+        }
+
+        const result = await pool.query(query, values);
+
+        if(result.rows.length === 0){
+            return res.status(404).json({ message: 'Order not found' });
+        }
+
+        res.status(200).json({ order: result.rows[0] });
+    } catch (err) {
+        next(err);
     }
 };
--- src/routes/order.routes.js
+++ src/routes/order.routes.js
@@ -1,8 +1,10 @@
 import express from 'express';
 import { authenticate, authorizeRoles } from '../middlewares/auth.middleware.js';
-import { createOrder } from '../auth/order.controller.js';
+import { createOrder, getOrderById, getOrders } from '../auth/order.controller.js';
 const orderRouter = express.Router();
 
 orderRouter.post('/createOrder', authenticate, authorizeRoles('USER'), createOrder);
+orderRouter.get('/getOrder', authenticate, getOrders);
+orderRouter.get('/getOrder/:id', authenticate, getOrderById);
 
 export default orderRouter;
